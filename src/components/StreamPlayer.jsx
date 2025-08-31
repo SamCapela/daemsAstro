57c0f85905c52164c5344ec8b3c729994daacc82
SamCapela <sam.capela@hotmail.fr>
2025-09-01 00:41:44 +0200

    fix twitch player

--- src/components/StreamPlayer.jsx
+++ src/components/StreamPlayer.jsx
@@ -3,12 +3,13 @@ import React from 'react';
 export default function StreamPlayer() {
   return (
     <div className="aspect-w-16 aspect-h-9">
-      <iframe
-        src="https://player.twitch.tv/?channel=daems_&parent=localhost"
-        height="100%"
-        width="100%"
-        allowFullScreen
-      ></iframe>
+          <iframe
+              src="https://player.twitch.tv/?channel=NomDeTaChaine&parent=daems-astro.vercel.app&parent=localhost"
+              height="480"
+              width="720"
+              allowfullscreen
+              frameborder="0">
+          </iframe>
     </div>
   );
 }
